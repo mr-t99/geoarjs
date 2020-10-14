75475a06c4045fe9fbfbea1a27a85ee58c9ad67b
mr-t99 <69566706+mr-t99@users.noreply.github.com>
2020-10-14 08:29:54 +0700

    Update places.js
    sasad

--- places.js
+++ places.js
@@ -4,33 +4,6 @@ window.onload = () => {
     let places = staticLoadPlaces();
     console.log(places);
     return renderPlaces(places);
-
-    // let method = 'dynamic';
-
-    // // if you want to statically add places, de-comment following line:
-    // method = 'static';
-    // if (method === 'static') {
-
-    // }
-
-    // if (method !== 'static') {
-    //     // first get current user location
-    //     return navigator.geolocation.getCurrentPosition(function (position) {
-
-    //         // than use it to load from remote APIs some places nearby
-    //         dynamicLoadPlaces(position.coords)
-    //             .then((places) => {
-    //                 renderPlaces(places);
-    //             })
-    //     },
-    //         (err) => console.error('Error in retrieving position', err),
-    //         {
-    //             enableHighAccuracy: true,
-    //             maximumAge: 0,
-    //             timeout: 27000,
-    //         }
-    //     );
-    // }
 };
 
 
@@ -66,47 +39,6 @@ function staticLoadPlaces() {
 
 
 
-
-
-
-
-
-
-
-
-
-// getting places from REST APIs
-function dynamicLoadPlaces(position) {
-    let params = {
-        radius: 300,    // search places not farther than this value (in meters)
-        clientId: 'PONER LOS CODIGOS',
-        clientSecret: 'PONER LOS CODIGOS',
-        version: '20300101',    // foursquare versioning, required but unuseful for this demo
-    };
-
-    // CORS Proxy to avoid CORS problems
-    let corsProxy = 'https://cors-anywhere.herokuapp.com/';
-
-    // Foursquare API
-    let endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
-        &ll=${position.latitude},${position.longitude}
-        &radius=${params.radius}
-        &client_id=${params.clientId}
-        &client_secret=${params.clientSecret}
-        &limit=15
-        &v=${params.version}`;
-    return fetch(endpoint)
-        .then((res) => {
-            return res.json()
-                .then((resp) => {
-                    return resp.response.venues;
-                })
-        })
-        .catch((err) => {
-            console.error('Error with places API', err);
-        })
-};
-
 function renderPlaces(places) {
     let scene = document.querySelector('a-scene');
 
@@ -115,11 +47,10 @@ function renderPlaces(places) {
         let longitude = place.location.lng;
 
         // add place name
-        let text = document.createElement('a-link');
-        text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        text.setAttribute('title', place.name);
-        // text.setAttribute('href', 'http://www.example.com/');
-        text.setAttribute('scale', '10 10 10');
+//         let text = document.createElement('a-link');
+//         text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+//         text.setAttribute('title', place.name);
+//         text.setAttribute('scale', '10 10 10');
 
 
         // add place icon
